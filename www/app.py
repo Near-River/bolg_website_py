c5c5d8001f6594823fd4306ba125038307a7eefa
Near_River <798563420@qq.com>
2016-05-17 18:11:47 +0800

    app

--- www/app.py
+++ www/app.py
@@ -8,19 +8,62 @@ async web application.
 '''
 
 import logging
-import asyncio
+import asyncio, time, os
 from aiohttp import web
+from www.coreweb import add_routes, add_static
+from www.middleware import logger_factory, response_factory
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+from conf.config import configs
+from www.orm import create_connection_pool
 
 logging.basicConfig(level=logging.INFO)
 
 
-def index(request):
-    return web.Response(body=b'<h1>Welcome</h1>')
+def init_jinja2(app, **kw):
+    logging.info('init jinja2...')
+    options = dict(
+        autoescape=kw.get('autoescape', True),
+        block_start_string=kw.get('block_start_string', '{%'),
+        block_end_string=kw.get('block_end_string', '%}'),
+        variable_start_string=kw.get('variable_start_string', '{{'),
+        variable_end_string=kw.get('variable_end_string', '}}'),
+        auto_reload=kw.get('auto_reload', True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
+
+
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    if delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    if delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 
 async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
+    await create_connection_pool(loop=loop, **configs.db)
+    app = web.Application(loop=loop, middlewares=[
+        logger_factory, response_factory
+    ])
+    init_jinja2(app=app, filters=dict(datetime=datetime_filter))
+    add_static(app=app)
+    add_routes(app=app, module_name='www.handlers')
     server = await loop.create_server(app.make_handler(), host='127.0.0.1', port=8080)
     logging.info('server started at http://127.0.0.1:8080')
     return server
