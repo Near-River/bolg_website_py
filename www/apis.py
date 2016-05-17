8ffb12a4c4dd0410cfd2e084c521d8ece7afe823
Near_River <798563420@qq.com>
2016-05-17 15:33:15 +0800

    api-error

--- www/apis.py
+++ www/apis.py
@@ -14,7 +14,6 @@ class APIError(Exception):
     """
     the base APIError which contains error(required), data(optional) and message(optional).
     """
-
     def __init__(self, error, data='', message=''):
         super(APIError, self).__init__(message)
         self.error = error
@@ -26,7 +25,6 @@ class APIValueError(APIError):
     """
     Indicate the input value has error or invalid. The data specifies the error field of input form.
     """
-
     def __init__(self, field, message=''):
         super(APIValueError, self).__init__('value:invalid', field, message)
 
@@ -35,7 +33,6 @@ class APIResourceNotFoundError(APIError):
     """
     Indicate the resource was not found. The data specifies the resource name.
     """
-
     def __init__(self, field, message=''):
         super(APIResourceNotFoundError, self).__init__('value:not found', field, message)
 
@@ -44,6 +41,5 @@ class APIPermissionError(APIError):
     """
     Indicate the api has no permission.
     """
-
     def __init__(self, message=''):
         super(APIPermissionError, self).__init__('permission:forbidden', 'permission', message)
